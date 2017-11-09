435ca07047afd7805f57a025f88449bc438ada94
Christoph Harms-Ensink <c.harms.ensink@gmail.com>
2017-11-09 11:13:22 +0100

    fiddle with gitlab-ci

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -5,12 +5,11 @@
 
 set -xe
 
-# Install git (the php image doesn't have it) which is required by composer
+# Install git (the php image doesn't have it) which is required by composer and other things we need
 apt-get update -yqq
-apt-get install git libxml2-dev -yqq
+apt-get install git libxml2-dev wget -yqq
 
-# Install mysql driver
-# Here you can install any other extension that you need
+# Install php extensions
 docker-php-ext-install pdo_mysql
 docker-php-ext-install mbstring
 docker-php-ext-install tokenizer
